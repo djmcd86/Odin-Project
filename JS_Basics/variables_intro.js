7a28d7565ec9f3463516d564a33ee13496049654
Dean McDonald <dean.j.mcdonald@gmail.com>
2018-08-29 15:36:13 +0930

    js functions

---
+++ JS_Basics/variables_intro.js
@@ -0,0 +1,20 @@
+//Using let
+let message;
+message = 'hello';
+alert(message); //'hello'
+
+//const is obviously a constant named in capitals and underscores:
+const COLOR_RED = "#F00";
+//this is done for a constant that you already know the value of
+//other constants are just camleCase
+
+//TASKS
+let admin;
+let name;
+
+name = 'John';
+admin = name;
+alert(admin);
+
+let ourPlanet;
+ourPlanet = 'Earth';
